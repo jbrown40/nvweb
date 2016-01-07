31230eea18b9d7516068741e0e1f1f38e80e5cf0
jbrown40 <jessicabrown@dhcp80ff31c4.dynamic.uiowa.edu>
2016-01-07 12:18:11 -0600

    create new fields

--- mysite/code/HomePage.php
+++ mysite/code/HomePage.php
@@ -3,6 +3,38 @@
 //model: contains all the custom database fields, data relationships, and functionality that can be expressed across multiple templates
 class HomePage extends Page{
 
+	private static $db = array (
+
+		'InformationOne' => 'Varchar(225)',
+	    'InformationTwo' => 'Varchar(225)',
+	    'InformationThree' => 'Varchar(225)', 
+
+	);
+
+	private static $has_one = array(
+		'ImageOne' => 'Image',
+		'ImageTwo' => 'Image',
+		'ImageThree' => 'Image',
+
+	);
+
+ 	 public function getCMSFields() {
+    	$fields = parent::getCMSFields();
+
+
+			$fields->addFieldToTab('Root.Main', TextareaField::create('InformationOne'), 'Content');   
+			$fields->addFieldToTab('Root.Main', new UploadField('ImageOne', 'Image'), 'Content');
+
+    		$fields->addFieldToTab('Root.Main', TextareaField::create('InformationTwo'), 'Content');
+ 			$fields->addFieldToTab('Root.Main', new UploadField('ImageTwo', 'Image'), 'Content');
+
+    		$fields->addFieldToTab('Root.Main', TextareaField::create('InformationThree'), 'Content');
+			$fields->addFieldToTab('Root.Main', new UploadField('ImageThree', 'Image'), 'Content');
+
+
+    	return $fields;
+  	}
+
 }
 
 //every page type has to have one
